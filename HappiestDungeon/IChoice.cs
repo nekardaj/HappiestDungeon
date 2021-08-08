d732d94620913b8e0c5c00eddd7d9021384cb188
Honzanek <honzanek111@gmail.com>
2021-08-08 23:58:39 +0200

    Terminal communication

---
+++ HappiestDungeon/IChoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappiestDungeon
+{
+    interface IChoice
+    {
+        /// <summary>
+        /// Asks player to choose from options that were added after last reset.
+        /// </summary>
+        /// <returns>
+        /// zero based number of possibility user choosed
+        /// ordered from first added choice to last
+        /// </returns>
+        int GetChoice(string promt);
+        void AddChoice(ILogging choice);
+        /// <summary>
+        /// empties list of posibilities
+        /// </summary>
+        void ResetChoices();
+        
+    }
+}
---
+++ HappiestDungeon/TerminalInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappiestDungeon
+{
+    class TerminalInput : IChoice
+    {
+        List<ILogging> Choices = new List<ILogging> { };
+        public void AddChoice(ILogging choice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetChoice(string prompt)
+        {
+            for (int i = 0; i < Choices.Count; i++)
+            {
+                Console.WriteLine((i + 1)+ ") " + Choices[i].ReturnDescription());
+            }
+            int retval = -1;
+            do
+            {
+                Console.WriteLine(prompt);
+                int.TryParse(Console.ReadLine(), out retval);
+            }
+            while (retval < 1 || retval > Choices.Count + 1);
+                
+            
+            return retval;
+        }
+
+        public void ResetChoices() //removes old choices
+        {
+            Choices.Clear();
+        }
+    }
+}
